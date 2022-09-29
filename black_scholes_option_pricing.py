d682486a869b427b1e50e7dd9070b37e6b445d2f
tahmidawal <98552052+tahmidawal@users.noreply.github.com>
2022-09-28 21:28:26 -0400

    Create black_scholes_option_pricing.py

---
+++ black_scholes_option_pricing.py
@@ -0,0 +1,14 @@
+
+
+def blackScholes(r, S, K, T, sigma, type="c"):
+    "Calculate BS price of call/put"
+    d1 = (np.log(S/K) + (r + sigma**2/2)*T)/(sigma*np.sqrt(T))
+    d2 = d1 - sigma*np.sqrt(T)
+    try:
+        if type == "c":
+            price = S*norm.cdf(d1, 0, 1) - K*np.exp(-r*T)*norm.cdf(d2, 0, 1)
+        elif type == "p":
+            price = K*np.exp(-r*T)*norm.cdf(-d2, 0, 1) - S*norm.cdf(-d1, 0, 1)
+        return price
+    except:
+        print("Error. Please check input values")
